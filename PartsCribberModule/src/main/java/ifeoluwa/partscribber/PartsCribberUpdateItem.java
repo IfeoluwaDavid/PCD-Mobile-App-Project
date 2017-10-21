038439c72c76a833cb3fc2221733d7a33d9f671e
Tosino45 <Ajayi.oluwatosin5@gmail.com>
2017-10-21 13:16:08 -0400

    Edit Individual Item Information

---
+++ PartsCribberModule/src/main/java/ifeoluwa/partscribber/PartsCribberUpdateItem.java
@@ -0,0 +1,13 @@
+package ifeoluwa.partscribber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartsCribberUpdateItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.partscribber_updateitem);
+    }
+}
